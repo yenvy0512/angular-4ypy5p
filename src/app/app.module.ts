41d3cd848fca5eeaf9440aab99d2a673a6cdd3a5
yenvy0512 <kieuyenvy0512@gmail.com>
2020-04-16 19:24:23 +0700

    3 files modified, 18 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { AdminComponent } from './backend/admin/admin.component';
+import { ProductComponent } from './backend/product/product.component';
+import { DashboardComponent } from './backend/dashboard/dashboard.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, AdminComponent, ProductComponent, DashboardComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
