de37aa6b4662442ad4ed9dcba20b096f22c4a335
yenvy0512 <kieuyenvy0512@gmail.com>
2020-04-17 21:47:40 +0700

    8 files modified, 7 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { AdminComponent } from './backend/admin/admin.component';
@@ -9,7 +9,7 @@ import { ProductComponent } from './backend/product/product.component';
 import { DashboardComponent } from './backend/dashboard/dashboard.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule,AppRoutingModule ],
   declarations: [ AppComponent, HelloComponent, AdminComponent, ProductComponent, DashboardComponent ],
   bootstrap:    [ AppComponent ]
 })
